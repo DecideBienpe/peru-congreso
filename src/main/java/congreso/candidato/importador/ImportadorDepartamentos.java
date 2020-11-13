9497bc827f9077747b28874a9eae05e61eea7162
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-11-13 11:30:50 +0000

    lista de candidatos por region obtenida

--- src/main/java/congreso/candidato/importador/ImportadorDepartamentos.java
+++ src/main/java/congreso/candidato/importador/ImportadorDepartamentos.java
@@ -3,8 +3,9 @@ package congreso.candidato.importador;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 
@@ -14,31 +15,50 @@ public class ImportadorDepartamentos {
     final var baseUrl = "http://peruvotoinformado.com/public";
     var doc = Jsoup.connect(baseUrl + "/2020").get();
     var listaDepartamentos = doc.body().select("li.list-group-item");
-    var map =
+    var refRegiones =
         listaDepartamentos
             .stream()
             .map(item -> item.select("a"))
             .collect(Collectors.toMap(a -> a.text().trim(), a -> a.attr("href").trim()));
 
-    var refCongresistaPorRegion = new LinkedHashMap<>();
+    var refCongresistaPorRegion = new LinkedHashMap<String, List<String>>();
 
-    map.forEach((region, href) -> {
+    refRegiones.forEach((region, href) -> {
       try {
-        var docRegion = Jsoup.connect(baseUrl + href).get();
 //        Files.writeString(Path.of("target/candidatos/departamentos/" + region + ".html"),
 //            docRegion.toString());
-        var cards = docRegion.select("div.card-body");
-        var refCandidatos = cards
-            .stream()
-            .map(c -> c.select("a").attr("href"))
-            .collect(Collectors.toList());
-        refCongresistaPorRegion.put(region, refCandidatos);
+        int i = 0;
+        int page = 1;
+        do {
+          var docRegion = Jsoup.connect(baseUrl + href + "?page=" + page).get();
+          var cards = docRegion.select("div.card-body");
+          i = cards.size();
+          var refCandidatos = cards
+              .stream()
+              .map(c -> c.select("a").attr("href"))
+              .collect(Collectors.toList());
+          var list = refCongresistaPorRegion.getOrDefault(region, new ArrayList<>());
+          list.addAll(refCandidatos);
+          refCongresistaPorRegion.put(region, list);
+          System.out.println(region + ": candidatos obtenidos " + list.size());
+          page ++;
+        } while (i > 0);
       } catch (IOException e) {
         e.printStackTrace();
       }
     });
 
-
+    refCongresistaPorRegion.forEach((region, refs) -> {
+      var buffer = new StringBuffer();
+      try {
+        refs.forEach(r -> buffer.append(String.format("%s%n", r)));
+        Files.writeString(
+            Path.of("static/regiones/" + region.replace(" ", "-") + ".txt"),
+            buffer.toString());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
   }
 
 }
